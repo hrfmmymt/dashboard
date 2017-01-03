fcdec90fda0465df966c43646f6c94834b0e8396
hrfmmymt <hrfmmymt@gmail.com>
2017-01-03 23:34:49 +0900

    add dev_process

--- main.rb
+++ main.rb
@@ -46,7 +46,8 @@ put "/:id/edit" do
     :content_detail => params[:content_detail],
     :device => params[:device],
     :langs => params[:langs],
-    :tools => params[:tools]
+    :tools => params[:tools],
+    :dev_process => params[:devs]
   })
   redirect "/"
 end
